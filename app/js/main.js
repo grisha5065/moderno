646a0f464def77fe81d819b6aec9e220e940f0c2
grisha5065 <shun.98@mail.ru>
2021-03-08 13:42:51 +0500

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function(){
 
-
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -11,12 +10,39 @@ $(function(){
           dots: true,
           arrows: false,
           slidesToShow: 4,
-          slidesToScroll: 4,
-          
+          slidesToScroll: 4,    
       });
 
+      // $(".js-range-slider").ionRangeSlider({
+      //     type: "double",
+      //     min: 0,
+      //     max: 1000,
+      //     from: 0,
+      //     to: 600,
+      //     prefix: "$"
+      // });
 
+      $('.icon-th-list').on('click', function(){
+          $('.product__item').addClass('list');
+          $('.icon-th-list').addClass('active');
+          $('.icon-th-large').removeClass('active');
+      });
+      $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+      });
     
-    var mixer = mixitup('.products__inner-box');
+
+
+      $('.menu__btn').on('click',  function(){
+          $('.menu__list').slideToggle();
+      });
+
+      $('.header__btn-menu').on('click',  function() {
+        $('.header__box').toggleClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box'); //  всегда писать последним
 
 });
