ac888f7713ac879db1c252cbd1b8758a20e045d5
grisha5065 <shun.98@mail.ru>
2021-03-19 21:13:14 +0500

    added 404, contact, about

--- app/js/main.js
+++ app/js/main.js
@@ -32,10 +32,19 @@ $(function(){
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
       });
+    //tab start
+      $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+//tab end
     
-
-
-      $('.menu__btn').on('click',  function(){
+    
+        $('.menu__btn').on('click',  function(){
           $('.menu__list').slideToggle();
       });
 
